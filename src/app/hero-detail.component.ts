6e0de4e243f60e537c6c48b58bc975cdbd4c5414
Jim X. Lin <miljinx@gmail.com>
2017-07-10 16:10:10 -0700

    Add HeroService to retrieve mock data

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -4,6 +4,7 @@ import { Hero } from './hero';
 @Component({
   selector: 'hero-detail',
   templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent {
   @Input() hero: Hero;
---
+++ src/app/hero.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+import { Hero } from './hero'
+import { HEROES } from './mock-heroes'
+
+@Injectable()
+export class HeroService {
+  getHeroes(): Promise<Hero[]> {
+    // immediately resolved promise, simulate 0 latency
+    return Promise.resolve(HEROES);
+  }
+
+  getHeroesSlowly(): Promise<Hero[]> {
+    return new Promise(resolve => {
+      // simulate 2 s latency
+      setTimeout(() => resolve(this.getHeroes()), 2000);
+    })
+  }
+}
