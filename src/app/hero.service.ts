53d1f56bdc7f1f57a71b94e43b022a95dc9a4c16
Jim X. Lin <miljinx@gmail.com>
2017-07-11 15:22:11 -0700

    Add add and delete actions

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -13,17 +13,17 @@ export class HeroService {
 
   getHeroes(): Promise<Hero[]> {
     return this.http.get(this.heroesUrl)
-               .toPromise()
-               .then(response => response.json().data as Hero[])
-               .catch(this.handleError);
+      .toPromise()
+      .then(response => response.json().data as Hero[])
+      .catch(this.handleError);
   }
 
   getHero(id: number): Promise<Hero> {
     const url = `${this.heroesUrl}/${id}`
     return this.http.get(url)
-               .toPromise()
-               .then(response => response.json().data as Hero)
-               .catch(this.handleError);
+      .toPromise()
+      .then(response => response.json().data as Hero)
+      .catch(this.handleError);
   }
 
   private handleError(error: any): Promise<any> {
@@ -39,6 +39,22 @@ export class HeroService {
       .put(url, JSON.stringify(hero), {headers: this.headers})
       .toPromise()
       .then(() => hero)
+      .catch(this.handleError);
+  }
+
+  create(name: string): Promise<Hero> {
+    return this.http
+      .post(this.heroesUrl, JSON.stringify({name: name}), {headers: this.headers})
+      .toPromise()
+      .then(res => res.json().data as Hero)
+      .catch(this.handleError);
+  }
+
+  delete(id: number): Promise<void> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.delete(url, {headers: this.headers})
+      .toPromise()
+      .then(() => null)
       .catch(this.handleError);
   }
 }
